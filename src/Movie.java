fd5611976c24a40152cffafc9eac6bfc3e8687ed
EgpCardoso <egpcardoso@minha.fag.edu.br>
2025-11-25 22:24:11 -0300

    Commit 11

--- src/Movie.java
+++ src/Movie.java
@@ -1,30 +1,43 @@
 public class Movie {
-
-    public static final int  CHILDRENS = 2;
-    public static final int  REGULAR = 0;
-    public static final int  NEW_RELEASE = 1;
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
-        _priceCode = priceCode;
+        setPriceCode(priceCode);
     }
 
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
-    public String getTitle (){
+    public String getTitle() {
         return _title;
     }
 
+    // Mantemos aqui os métodos que ainda serão refatorados nos próximos passos
     public double getCharge(int daysRented) {
+        // por enquanto ainda usa switch, mas logo será movido para as subclasses de Price
         double result = 0;
         switch (getPriceCode()) {
             case REGULAR:
@@ -50,5 +63,4 @@ public class Movie {
         }
         return 1;
     }
-
 }
